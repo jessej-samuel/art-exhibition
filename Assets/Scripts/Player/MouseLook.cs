1d1f38b7f365623ada128116809087a1358a79a4
Sachin Kishan <sachinkishan02@gmail.com>
2021-10-04 21:57:17 +0530

    player movement (pc)

---
+++ Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity;
+
+    public Transform playerBody;
+
+    private float xRotation=0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis(("Mouse X"))*mouseSensitivity*Time.deltaTime;
+        float mouseY = Input.GetAxis(("Mouse Y"))*mouseSensitivity*Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -65f, 45f);
+        transform.localRotation=Quaternion.Euler(xRotation,0f,0f);
+        playerBody.Rotate(Vector3.up*mouseX);
+        
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    public CharacterController controller;
+    public float gravity = -9.81f;
+    private Vector3 v;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && v.y < 0f) v.y = -2f;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move*speed*Time.deltaTime);
+        v.y += gravity * Time.deltaTime;
+        controller.Move(v*Time.deltaTime);
+    }
+}
